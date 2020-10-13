4e5ac1b7dedfe1f80b6f0a261423144909485125
kevin-ingles <66982466+kevin-ingles@users.noreply.github.com>
2020-10-13 15:10:24 -0400

    Add files via upload

---
+++ 2d-rectangle-area.js
@@ -0,0 +1,9 @@
+function main(length, width) {
+    var area = 0;
+    var p = 0;
+    area = length * width;
+    p = 2 * length + 2 * width;
+    console.log(area);
+    console.log(p);
+}
+main(5,4);
